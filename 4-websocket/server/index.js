00cb0feb6f7c61a8186e428628fa9981877cbae7
Lam <daibanglam@gmail.com>
2021-11-12 20:39:36 +0700

    fix cannot read file content after changing data

--- 4-websocket/server/index.js
+++ 4-websocket/server/index.js
@@ -4,21 +4,24 @@ const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 3000 });
 const datafile = __dirname + '/../../data.txt';
 
-function sendData(ws) {
-  const data = fs.readFileSync(datafile, 'utf8');
+let lastMod;
+
+function watchFileChange(filename, cb) {
+  const mtime = fs.statSync(filename).mtime;
+  if (!lastMod || lastMod.getTime() !== mtime.getTime()) {
+    lastMod = mtime;
+    cb();
+  }
+  setTimeout(() => watchFileChange(filename, cb), 50);
+}
+
+function sendData(ws, filename) {
+  const data = fs.readFileSync(filename, 'utf8');
   ws.send(data);
   console.log(data);
 }
 
 wss.on('connection', (ws) => {
   console.log('connected');
-
-  sendData(ws);
-
-  fs.watch(datafile, 'utf8', (event) => {
-    if (event === 'change') {
-      console.log(`file changed`);
-      setTimeout(() => sendData(ws), 10);
-    }
-  });
+  watchFileChange(datafile, () => sendData(ws, datafile));
 });
