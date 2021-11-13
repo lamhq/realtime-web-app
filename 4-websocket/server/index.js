a647da613348b4145a6a5307f18cc0b933db0c78
Lam <daibanglam@gmail.com>
2021-11-13 17:16:21 +0700

    Web socket demo

--- 4-websocket/server/index.js
+++ 4-websocket/server/index.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 const { WebSocketServer } = require('ws');
-const { watchFileChange } = require('../../utils');
+const { watchFileChange, getFileContent } = require('../../utils');
 
 const wss = new WebSocketServer({ port: 3000 });
 const datafile = __dirname + '/../../data.txt';
 
-function sendData(ws, filename) {
-  const data = fs.readFileSync(filename, 'utf8');
-  ws.send(data);
-  console.log(data);
-}
-
 wss.on('connection', (ws) => {
+
   console.log('connected');
-  watchFileChange(datafile, () => sendData(ws, datafile));
+
+  // subscribe for file change
+  const unSubscribe = watchFileChange(datafile, () => {
+    ws.send(getFileContent(datafile));
+  });
+  
+  ws.onclose = () => {
+    console.log('disconnected');
+    unSubscribe();
+  };
 });
